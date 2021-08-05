13dcb7fce1b5753ed595ac3b5f0b1b2aaee65eaa
Dnicholas-GIT <dan1024@wildcats.unh.edu>
2021-08-05 15:31:19 -0400

    pygame dev

--- Pygame/pygame_test.py
+++ Pygame/pygame_test.py
@@ -7,27 +7,39 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('Runner', False, 'Red')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_xpos = 600
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (700,300))
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
+        if event.type == pygame.MOUSEMOTION:
+
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface,(300,50))
 
-    snail_xpos += -3
-    if snail_xpos <= -90:
-        snail_xpos = 900
 
-    screen.blit(snail_surface,(snail_xpos,265))
+    snail_rect.x -= 4
+    if snail_rect.x <= -100: snail_rect.left = 850
+    screen.blit(snail_surface, snail_rect)
+
+    screen.blit(player_surface, player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint(mouse_pos):
+        pygame.mouse.get_pressed()
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(45)
